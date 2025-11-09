7f6b43b4a78941886dca4304d90cf559e1d27ae2
Szokoly-Angyal Armand <szokoly-angyal.armand@outlook.com>
2025-11-09 18:51:19 +0100

    refactor: move save and tagging logic to routedisplay

--- src/components/RouteDisplay.tsx
+++ src/components/RouteDisplay.tsx
@@ -2,6 +2,13 @@ import type { GenericRoute, RouteStep } from "../datamodels/trip";
 import type { ReactNode } from "react";
 import { FlightOfferDisplay } from "./FlightOfferDisplay";
 import { GoogleMapsRouteDisplay } from "./GoogleRouteDisplay";
+import { RouteTagsDisplay, RouteTagsDropdown } from "./RouteTagsDisplay";
+import type { Tag } from "../datamodels/Tag";
+import { toastContext } from "./FlightPlannerToast";
+import React from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { logger } from "../logging/logger";
+import { saveRoute } from "../logic/queryBackend";
 
 function RouteDisplayStep({ step }: Readonly<{ step: RouteStep }>) {
   let content: ReactNode;
@@ -23,19 +30,79 @@ function RouteDisplayStep({ step }: Readonly<{ step: RouteStep }>) {
     );
   }
 
-  return (
-    content
-  );
+  return content;
 }
 
-export function RouteDisplay({ route }: Readonly<{ route: GenericRoute }>) {
+export function RouteDisplay({
+  route,
+  setRoute,
+}: Readonly<{ route: GenericRoute; setRoute: (route: GenericRoute) => void }>) {
   if (!route || route.steps.length === 0) {
     return <p>No route found. Try another search.</p>;
   }
 
-  return (
+  const queryClient = useQueryClient();
+
+  const { addToast } = React.useContext(toastContext);
+
+  const updateRouteTags = (newTags: Tag[]) => {
+    if (route) {
+      setRoute({ ...route, tags: newTags });
+    }
+    queryClient.invalidateQueries({ queryKey: ["getRoutes"] });
+  };
+
+  const onClickSave = async () => {
+    if (!route) {
+      logger.warn("No route to save.");
+      return;
+    }
+    // update route id here too
+    let route_id: number | undefined = undefined;
+    try {
+      route_id = await saveRoute(route);
+    } catch (e) {
+      addToast("danger", `Error saving route: ${(e as Error).message}`);
+      return;
+    }
+    if (route_id) addToast("success", "Route saved successfully");
+
+    let new_route = route;
+    new_route.id = String(route_id);
+    setRoute(new_route);
+    logger.info(`Route saved with ID: ${route_id}`);
+  };
+
+  const routeTagsDisplay = (
     <>
-      <h3>{route.searchStart} to {route.searchEnd}</h3>
+      <RouteTagsDisplay tags={route.tags} setTags={updateRouteTags} />
+      <RouteTagsDropdown
+        currentTags={route.tags}
+        setRouteTags={updateRouteTags}
+      />
+    </>
+  );
+
+  let saveButtonAndTags: ReactNode = <></>;
+
+  if (route && route.steps.length > 0) {
+    const buttonText = route.id ? "Update Route" : "Save Route";
+    saveButtonAndTags = (
+      <button
+        className="btn btn-primary align-self-start"
+        type="button"
+        onClick={onClickSave}
+      >
+        {buttonText}
+      </button>
+    );
+  }
+
+  const mainBody = (
+    <>
+      <h3>
+        {route.searchStart} to {route.searchEnd}
+      </h3>
       {route.steps.map((step: RouteStep, index: any) => {
         const key = `${step.travelMode}-${
           step.startLocation?.latitude ?? "na"
@@ -44,5 +111,15 @@ export function RouteDisplay({ route }: Readonly<{ route: GenericRoute }>) {
         return <RouteDisplayStep key={key} step={step} />;
       })}
     </>
+  );
+
+  return (
+    <div className="d-flex flex-column gap-3">
+      <div className="d-flex fley-row gap-1">
+        {routeTagsDisplay}
+        {saveButtonAndTags}
+      </div>
+      {mainBody}
+    </div>
   );
 }
--- src/components/RouteSearchPanel.tsx
+++ src/components/RouteSearchPanel.tsx
@@ -1,15 +1,15 @@
 import { type ReactNode } from "react";
-import { QueryClient, useMutation } from "@tanstack/react-query";
-import { apiFetch, saveRoute } from "../logic/queryBackend";
+import {
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { apiFetch } from "../logic/queryBackend";
 import { BACKEND_URL } from "../env";
 import { RouteDisplay } from "../components/RouteDisplay";
 import type { GenericRoute } from "../datamodels/trip";
 import { logger } from "../logging/logger";
 import React from "react";
 import { MultimodalRouteSearchControls } from "./MultiModalRouteSearchControls";
-import { RouteTagsDisplay, RouteTagsDropdown } from "./RouteTagsDisplay";
-import { toastContext } from "./FlightPlannerToast";
-import type { Tag } from "../datamodels/Tag";
 if (!BACKEND_URL) {
   logger.error("BACKEND_URL is not defined in environment variables.");
 }
@@ -17,15 +17,13 @@ if (!BACKEND_URL) {
 export function RouteSearchPanel() {
   const [route, setRoute] = React.useState<GenericRoute | undefined>(undefined);
 
-  const { addToast } = React.useContext(toastContext);
-
-  const queryClient = new QueryClient();
+  const queryClient = useQueryClient();
 
   let routeDisplayPanel: ReactNode;
 
   const mutation = useMutation({
     mutationFn: async (payload: any) => {
-      const data = await apiFetch<GenericRoute>(`${BACKEND_URL}/get-route`, {
+      const data = await apiFetch<GenericRoute>(`${BACKEND_URL}/get-test-route`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
@@ -38,56 +36,12 @@ export function RouteSearchPanel() {
     },
   });
 
-  const onClickSave = async () => {
-    if (!route) {
-      logger.warn("No route to save.");
-      return;
-    }
-    // update route id here too
-    let route_id: number | undefined = undefined;
-    try {
-      route_id = await saveRoute(route);
-    } catch (e) {
-      addToast("danger", `Error saving route: ${(e as Error).message}`);
-      return;
-    }
-    if (route_id) addToast("success", "Route saved successfully");
-
-    let new_route = route;
-    new_route.id = String(route_id);
-    setRoute(new_route);
-    logger.info(`Route saved with ID: ${route_id}`);
-  };
-
-  let saveButtonAndTags: ReactNode;
-
-  if (route && route.steps.length > 0) {
-    saveButtonAndTags = (
-      <button
-        className="btn btn-primary align-self-start"
-        type="button"
-        onClick={onClickSave}
-      >
-        Save This Route
-      </button>
-    );
-  }
-
   const handleSearchClick = (
     origin: string,
     destination: string,
     date: string
   ) => {
     mutation.mutate({ origin, destination, date });
-  };
-
-  let routeTagsDisplay = <></>;
-
-  const updateRouteTags = (newTags: Tag[]) => {
-    if (route) {
-      setRoute({ ...route, tags: newTags });
-    }
-    queryClient.invalidateQueries({ queryKey: ["getRoutes"] });
   };
 
   if (mutation.isPending) {
@@ -100,15 +54,7 @@ export function RouteSearchPanel() {
     );
     logger.error("Error fetching planned route:", mutation.error);
   } else if (route && route.steps.length > 0) {
-    routeTagsDisplay = (
-      <>
-        <RouteTagsDisplay tags={route.tags} setTags={updateRouteTags} />
-        <RouteTagsDropdown currentTags={route.tags}
-          setRouteTags={updateRouteTags}
-        />
-      </>
-    );
-    routeDisplayPanel = <RouteDisplay route={route} />;
+    routeDisplayPanel = <RouteDisplay route={route} setRoute={setRoute} />;
   } else {
     routeDisplayPanel = <p>No route planned yet.</p>;
   }
@@ -119,11 +65,7 @@ export function RouteSearchPanel() {
         searchMultiModalRoute={handleSearchClick}
       />
       <br />
-      <div className = "d-flex flex-row gap-3">
-        {routeTagsDisplay}
-        {saveButtonAndTags}
-      </div>
-        {routeDisplayPanel}
+      {routeDisplayPanel}
     </div>
   );
 
